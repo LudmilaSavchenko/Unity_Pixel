747dbe2d5302a7b1aba2d0854404823e5914adcb
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2019-12-14 23:52:03 +0300

    hw 1 v4

--- Platformer/Platformer/Assets/Resources/Scripts/Player.cs
+++ Platformer/Platformer/Assets/Resources/Scripts/Player.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector2.left);
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
         }
     }
 }
