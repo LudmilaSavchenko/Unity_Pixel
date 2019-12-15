8d733e09ccfd2e40cdfca44d676f5f69ba2984ae
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2019-12-15 22:19:43 +0300

    Cat platformer created v1

---
+++ Platformer/Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float force;
+    public Rigidbody2D rigidbody;
+    private bool isGrounded = false;
+    public Transform groundCheck;
+    private float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        }
+    }
+}
