db7c6e87a262151b5501a55de9ddea6a55549d03
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2019-12-24 00:14:41 +0300

    made enemy + his movements

---
+++ Platformer/Platformer/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public GameObject leftBorder;
+    public GameObject rightBorder;
+    public Rigidbody2D rigidbody;
+
+    public bool isRightDirection;
+    public float speed;
+
+    private void Update()
+    { 
+        if (isRightDirection)
+        {
+            rigidbody.velocity = Vector2.right * speed;
+            if (transform.position.x > rightBorder.transform.position.x)
+            {
+                isRightDirection = !isRightDirection;
+            }
+        }
+        else
+        {
+            rigidbody.velocity = Vector2.left * speed;
+            if (transform.position.x < leftBorder.transform.position.x)
+            {
+                isRightDirection = !isRightDirection;
+            }
+        }
+    }
+}
+
