53e0e79ee0bd7661547ba2ac87046f0b7079f032
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2020-09-08 23:57:25 +0300

    UI: done smooth healthbar

--- Platformer/Platformer/Assets/Scripts/HealthBar.cs
+++ Platformer/Platformer/Assets/Scripts/HealthBar.cs
@@ -6,17 +6,29 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image health;
+    [SerializeField] private float delta;
     private float healthValue;
+    private float currentHealth;
     private Player player;
 
     void Start()
     {
         player = Player.Instance; // FindObjectOfType<Player>();
+        //delta = player.Health.CurrentHealth / 100.0f;
     }
 
     
     void Update()
     {
-        health.fillAmount = player.Health.CurrentHealth / 100.0f; 
+        currentHealth = player.Health.CurrentHealth / 100.0f;
+        if (currentHealth > healthValue)
+            healthValue += delta;
+        else if (currentHealth < healthValue)
+            healthValue -= delta;
+        else if (Mathf.Abs(currentHealth - healthValue) < delta)
+            healthValue = currentHealth;
+
+        //health.fillAmount = player.Health.CurrentHealth / 100.0f;
+        health.fillAmount = healthValue;
     }
 }
